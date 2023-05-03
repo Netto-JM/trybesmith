a081a379c50ceba570684d774a7d37bcdf83bbf1
NettoJM <nettojm86@gmail.com>
2023-05-03 15:54:45 -0300

    Implement statusCodes

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,28 +1,25 @@
 import { Request, Response } from 'express';
 import { productService } from '../services';
-
-const HTTP_OK_STATUS = 200;
-const HTTP_CREATED_STATUS = 201;
-const HTTP_NOT_FOUND_STATUS = 404;
+import statusCodes from '../statusCodes';
 
 async function createProduct(req: Request, res: Response) {
   const { name, amount } = req.body;
   const newProduct = await productService.createProduct(name, amount);
-  return res.status(HTTP_CREATED_STATUS).json(newProduct);
+  return res.status(statusCodes.CREATED).json(newProduct);
 }
 
 async function listProducts(_req: Request, res: Response) {
   const products = await productService.findAll();
-  return res.status(HTTP_OK_STATUS).json(products);
+  return res.status(statusCodes.OK).json(products);
 }
 
 async function getProductById(req: Request, res: Response) {
   const { id } = req.params;
   const product = await productService.findById(+id);
 
-  if (product) return res.status(HTTP_OK_STATUS).json(product);
+  if (product) return res.status(statusCodes.OK).json(product);
 
-  res.status(HTTP_NOT_FOUND_STATUS).send('Product not found');
+  return res.status(statusCodes.NOT_FOUND).send('Product not found');
 }
 
 export default {
---
+++ src/statusCodes.ts
@@ -0,0 +1,7 @@
+const statusCodes = {
+  OK: 200,
+  CREATED: 201,
+  NOT_FOUND: 404,
+};
+
+export default statusCodes;
