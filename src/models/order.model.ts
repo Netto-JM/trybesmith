5274847e25c5151b8977f4c74e429d2b3a5011f0
NettoJM <nettojm86@gmail.com>
2023-05-04 01:03:25 -0300

    Uppercased "as" to "AS"

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -4,7 +4,7 @@ import connection from './connection';
 
 async function findAll(): Promise<Order[]> {
   const [rows] = await connection.execute<RowDataPacket[]>(
-    `SELECT od.id, od.user_id as userId, JSON_ARRAYAGG(pd.id) AS productsIds
+    `SELECT od.id, od.user_id AS userId, JSON_ARRAYAGG(pd.id) AS productsIds
     FROM Trybesmith.orders AS od
     JOIN Trybesmith.products AS pd ON od.id = pd.order_id
     GROUP BY od.id;`,
