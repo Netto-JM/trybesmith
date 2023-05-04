879d91bab88af15366db1ee06ccb80688782bce0
NettoJM <nettojm86@gmail.com>
2023-05-03 21:29:05 -0300

    Add "user" Router

---
+++ src/routes/user.router.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import { userController } from '../controllers';
+
+const userRouter = Router();
+userRouter.post('/', userController.createUser);
+
+export default userRouter;
